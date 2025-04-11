5140e550e2a7c8987b369d8e6dbc6fa9065d05a7
chlee <chlee@student.42seoul.kr>
2025-04-12 04:04:55 +0900

    modify: reservation controller, request to fit test

--- src/main/java/com/yourssu/roomescape/reservation/ReservationRequest.java
+++ src/main/java/com/yourssu/roomescape/reservation/ReservationRequest.java
@@ -21,4 +21,8 @@ public class ReservationRequest {
     public Long getTime() {
         return time;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    } // is proper?
 }
