4df69b5129b7fe453048c3be9154ed193c79d963
chlee <chlee@student.42seoul.kr>
2025-04-12 04:05:26 +0900

    add another file

---
+++ src/main/java/com/yourssu/roomescape/WebConfig.java
@@ -0,0 +1,33 @@
+package com.yourssu.roomescape;
+
+import com.yourssu.roomescape.auth.LoginMemberArgumentResolver;
+import com.yourssu.roomescape.auth.RoleCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final RoleCheckInterceptor roleCheckInterceptor;
+
+    public WebConfig(LoginMemberArgumentResolver loginMemberArgumentResolver, RoleCheckInterceptor roleCheckInterceptor) {
+        this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.roleCheckInterceptor = roleCheckInterceptor;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(roleCheckInterceptor)
+                .addPathPatterns("/admin/**");
+    }
+}
--- src/test/java/com/yourssu/roomescape/MissionStepTest.java
+++ src/test/java/com/yourssu/roomescape/MissionStepTest.java
@@ -1,10 +1,16 @@
 package com.yourssu.roomescape;
 
+import com.yourssu.roomescape.infrastructure.JwtTokenProvider;
+import com.yourssu.roomescape.reservation.ReservationResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -17,6 +23,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    // todo: moving this method to jwtTokenProvider
     @Test
     void 일단계() {
         Map<String, String> params = new HashMap<>();
@@ -32,7 +42,69 @@ public class MissionStepTest {
                 .extract();
 
         String token = response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
-
         assertThat(token).isNotBlank();
+
+        ExtractableResponse<Response> checkResponse = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("token", token)
+                .when().get("/login/check")
+                .then().log().all()
+                .statusCode(200)
+                .extract();
+
+        assertThat(checkResponse.body().jsonPath().getString("name")).isEqualTo("어드민");
+    }
+
+    @Test
+    void 이단계() {
+        String token = jwtTokenProvider.createTokenToTest("admin@email.com", "password");  // 일단계에서 토큰을 추출하는 로직을 메서드로 따로 만들어서 활용하세요.
+
+        Map<String, String> params = new HashMap<>();
+        params.put("date", "2024-03-01");
+        params.put("time", "1");
+        params.put("theme", "1");
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .post("/reservations")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(201);
+        assertThat(response.as(ReservationResponse.class).getName()).isEqualTo("어드민");
+
+        params.put("name", "브라운");
+
+        ExtractableResponse<Response> adminResponse = RestAssured.given().log().all()
+                .body(params)
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .post("/reservations")
+                .then().log().all()
+                .extract();
+
+        assertThat(adminResponse.statusCode()).isEqualTo(201);
+        assertThat(adminResponse.as(ReservationResponse.class).getName()).isEqualTo("브라운");
+    }
+
+    @Test
+    void 삼단계() {
+        String brownToken = jwtTokenProvider.createTokenToTest("brown@email.com", "password");
+        System.out.println("token : " + brownToken);
+        RestAssured.given().log().all()
+                .cookie("token", brownToken)
+                .get("/admin")
+                .then().log().all()
+                .statusCode(401);
+
+        String adminToken = jwtTokenProvider.createTokenToTest("admin@email.com", "password");
+
+        RestAssured.given().log().all()
+                .cookie("token", adminToken)
+                .get("/admin")
+                .then().log().all()
+                .statusCode(200);
     }
 }
