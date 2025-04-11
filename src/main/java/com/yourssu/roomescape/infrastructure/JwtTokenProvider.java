2cd52b56fd2e6c0af7d01a9814b1643de63b020d
chlee <chlee@student.42seoul.kr>
2025-04-12 04:02:49 +0900

    add: JWT token provider

---
+++ src/main/java/com/yourssu/roomescape/infrastructure/JwtTokenProvider.java
@@ -0,0 +1,40 @@
+package com.yourssu.roomescape.infrastructure;
+
+import com.yourssu.roomescape.member.Member;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+// code from spring-learning-test
+@Component
+public class JwtTokenProvider {
+    @Value("${roomescape.auth.jwt.secret}")
+    private String secretKey;
+    public String createToken(Member member) {
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .claim("email", member.getEmail())
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+
+    public String createTokenToTest(String email, String password) {
+        return Jwts.builder()
+                .setSubject("totest") // to mission 1-2
+                .claim("email", email)
+                .claim("password", password)
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+
+    public String getPayload(String token) {
+        return Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody().get("email", String.class); // before id, but change to name ; cause test
+    }
+}
