d3ab397b925959ef33913641da182e1adbc76a10
chlee <chlee@student.42seoul.kr>
2025-04-12 04:04:55 +0900

    modify: reservation controller, request to fit test

--- src/main/java/com/yourssu/roomescape/reservation/ReservationController.java
+++ src/main/java/com/yourssu/roomescape/reservation/ReservationController.java
@@ -1,18 +1,24 @@
 package com.yourssu.roomescape.reservation;
 
+import com.yourssu.roomescape.auth.LoginCheckResponse;
+import com.yourssu.roomescape.auth.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ReservationController {
 
     private final ReservationService reservationService;
+    private final LoginService loginService;
 
-    public ReservationController(ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService, LoginService loginService) {
         this.reservationService = reservationService;
+        this.loginService = loginService;
     }
 
     @GetMapping("/reservations")
@@ -21,15 +27,33 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity create(@RequestBody ReservationRequest reservationRequest) {
-        if (reservationRequest.getName() == null
-                || reservationRequest.getDate() == null
-                || reservationRequest.getTheme() == null
-                || reservationRequest.getTime() == null) {
+    // why handlerMethodArgumentResolver not invoked?
+    // use HttpServletRequest to pass test only
+    // todo: modify to resolver until sunday
+    public ResponseEntity create(@RequestBody ReservationRequest reservationRequest, HttpServletRequest request) {
+        if (reservationRequest.getDate() == null
+            || reservationRequest.getTheme() == null
+            || reservationRequest.getTime() == null) {
             return ResponseEntity.badRequest().build();
         }
-        ReservationResponse reservation = reservationService.save(reservationRequest);
+        else if (reservationRequest.getName() == null) {
+            if (request.getCookies() == null) {
+                return ResponseEntity.noContent().build();
+            }
 
+            String token = Arrays.stream(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals("token"))
+                    .map(cookie -> cookie.getValue())
+                    .findFirst().orElse("");
+
+            if (token.isBlank()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            String name = loginService.checkLogin(token);
+            reservationRequest.setName(name);
+        }
+        ReservationResponse reservation = reservationService.save(reservationRequest);
         return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).body(reservation);
     }
 
