45d992f88593eca72eb3b7c43c29c199c85e04b6
chlee <chlee@student.42seoul.kr>
2025-04-14 00:53:19 +0900

    refactor: make requestDTO immutable using @JsonCreator

--- src/main/java/com/yourssu/roomescape/member/MemberRequest.java
+++ src/main/java/com/yourssu/roomescape/member/MemberRequest.java
@@ -1,9 +1,22 @@
 package com.yourssu.roomescape.member;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MemberRequest {
-    private String name;
-    private String email;
-    private String password;
+    private final String name;
+    private final String email;
+    private final String password;
+
+    @JsonCreator
+    public MemberRequest(
+            @JsonProperty("name") String name,
+            @JsonProperty("email") String email,
+            @JsonProperty("password") String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getName() {
         return name;
--- src/main/java/com/yourssu/roomescape/reservation/ReservationRequest.java
+++ src/main/java/com/yourssu/roomescape/reservation/ReservationRequest.java
@@ -1,10 +1,25 @@
 package com.yourssu.roomescape.reservation;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ReservationRequest {
-    private String name;
-    private String date;
-    private Long theme;
-    private Long time;
+    private final String name;
+    private final String date;
+    private final Long theme;
+    private final Long time;
+
+    @JsonCreator
+    public ReservationRequest(
+            @JsonProperty("name") String name,
+            @JsonProperty("date") String date,
+            @JsonProperty("theme") Long theme,
+            @JsonProperty("time") Long time) {
+        this.name = name;
+        this.date = date;
+        this.theme = theme;
+        this.time = time;
+    }
 
     public String getName() {
         return name;
