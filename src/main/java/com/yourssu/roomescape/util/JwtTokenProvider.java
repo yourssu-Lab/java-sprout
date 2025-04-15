e5e0eab5b025faa2d01113d228eb408f84b06bdf
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-04-15 22:36:24 +0900

    refactor(jwt): JWT 발급 구조를 Member 중심으로 개선하고 유틸성 클래스로 책임 분리

---
+++ src/main/java/com/yourssu/roomescape/util/JwtTokenProvider.java
@@ -0,0 +1,55 @@
+package com.yourssu.roomescape.util;
+
+import com.yourssu.roomescape.member.Member;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key key;
+    private final long validityInMilliseconds;
+
+    public JwtTokenProvider(
+            @Value("${roomescape.auth.jwt.secret}") String secretKey,
+            @Value("${security.jwt.token.expire-length}") long validityInMilliseconds
+    ) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
+
+    public String createToken(Member member) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(member.getEmail())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key)
+                .compact();
+    }
+
+    public String getEmail(String token) {
+        return getClaims(token).getSubject(); 
+    }
+
+    public String getMemberName(String token) {
+        return getClaims(token).get("name", String.class);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
