1ac6785b911db0c00caca72adcc84f11d2971068
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-04-15 22:26:34 +0900

    refactor(member): loginCheck 중복 API 제거 (AuthController로 책임 이전)

--- src/main/java/com/yourssu/roomescape/member/MemberController.java
+++ src/main/java/com/yourssu/roomescape/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.yourssu.roomescape.member;
 
+import com.yourssu.roomescape.auth.LoginCheckResponse;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -19,37 +19,37 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequest loginRequest, HttpServletResponse httpServletResponse){
-        LoginResponse loginResponse = memberService.login(loginRequest); // TODO: 여기 변수명과 반환값 이름에 대해서 생각해보기
-
-        Cookie cookie = new Cookie("token",loginResponse.getToken()); // TODO: constant 프로퍼티로 관리
-        httpServletResponse.addCookie(cookie);
-
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/login/check")
-    public ResponseEntity loginCheck(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-
-        // TODO: 빈문자열 관련 예외처리
-        String token = "";
-
-        // TODO: stream 사용해보기
-        for (Cookie cookie : cookies) {
-            if(cookie.getName().equals("token")) {
-                token = cookie.getValue();
-            }
-        }
-        String name = memberService.loginCheck(token);
-        return ResponseEntity.ok().body(new LoginCheckResponse(name));
-    }
-        @PostMapping("/members")
-    public ResponseEntity createMember(@RequestBody MemberRequest memberRequest) {
-        MemberResponse member = memberService.createMember(memberRequest);
-        return ResponseEntity.created(URI.create("/members/" + member.getId())).body(member);
-    }
+//    @PostMapping("/login")
+//    public ResponseEntity login(@RequestBody LoginRequest loginRequest, HttpServletResponse httpServletResponse){
+//        LoginResponse loginResponse = memberService.login(loginRequest); // TODO: 여기 변수명과 반환값 이름에 대해서 생각해보기
+//
+//        Cookie cookie = new Cookie("token",loginResponse.getToken()); // TODO: constant 프로퍼티로 관리
+//        httpServletResponse.addCookie(cookie);
+//
+//        return ResponseEntity.ok().build();
+//    }
+//
+//    @GetMapping("/login/check")
+//    public ResponseEntity loginCheck(HttpServletRequest request) {
+//        Cookie[] cookies = request.getCookies();
+//
+//        // TODO: 빈문자열 관련 예외처리
+//        String token = "";
+//
+//        // TODO: stream 사용해보기
+//        for (Cookie cookie : cookies) {
+//            if(cookie.getName().equals("token")) {
+//                token = cookie.getValue();
+//            }
+//        }
+//        String name = memberService.loginCheck(token);
+//        return ResponseEntity.ok().body(new LoginCheckResponse(name));
+//    }
+//        @PostMapping("/members")
+//    public ResponseEntity createMember(@RequestBody MemberRequest memberRequest) {
+//        MemberResponse member = memberService.createMember(memberRequest);
+//        return ResponseEntity.created(URI.create("/members/" + member.getId())).body(member);
+//    }
 
     @PostMapping("/logout")
     public ResponseEntity logout(HttpServletResponse response) {
--- src/main/java/com/yourssu/roomescape/member/MemberService.java
+++ src/main/java/com/yourssu/roomescape/member/MemberService.java
@@ -1,6 +1,7 @@
 package com.yourssu.roomescape.member;
 
-import com.yourssu.roomescape.JwtTokenProvider;
+import com.yourssu.roomescape.exception.MemberNotFoundException;
+import com.yourssu.roomescape.util.JwtTokenProvider;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,18 +17,5 @@ public class MemberService {
     public MemberResponse createMember(MemberRequest memberRequest) {
         Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
-    }
-
-    public LoginResponse login(LoginRequest loginRequest) {
-        Member member = memberDao.findByEmailAndPassword(loginRequest.getEmail(), loginRequest.getPassword()); // TODO: 유저 찾기 여기 유저 없으면 예외 처리 어떻게 할지 고민해보기
-        String token = jwtTokenProvider.createToken(member.getEmail());
-        return new LoginResponse(token);
-    }
-
-    public String loginCheck(String token) {
-
-        String email = jwtTokenProvider.getPayload(token);
-        Member member = memberDao.findByEmail(email);
-        return member.getName();
     }
 }
