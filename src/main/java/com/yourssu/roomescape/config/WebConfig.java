784aad0f7a3f3628d9b548c867675a9b13c032be
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-04-15 22:38:08 +0900

    feat(auth): 인증이 필요한 API 요청에 대해 JWT 검증 인터셉터 적용

---
+++ src/main/java/com/yourssu/roomescape/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.yourssu.roomescape.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Autowired
+    public WebConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/login", "/login/check", "/h2-console/**", "/css/**", "/js/**", "/"); // 비보호 경로
+    }
+}
