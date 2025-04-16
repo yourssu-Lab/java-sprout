66271f36937bdf49966ce34851c604218d281272
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-04-16 18:46:11 +0900

    chore(config): WebMvcConfigurer에 AuthInterceptor 등록

--- src/main/java/com/yourssu/roomescape/config/WebConfig.java
+++ src/main/java/com/yourssu/roomescape/config/WebConfig.java
@@ -5,7 +5,7 @@ import com.yourssu.roomescape.member.MemberDao;
 import com.yourssu.roomescape.util.JwtTokenProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.List;
 
@@ -14,14 +14,22 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final MemberDao memberDao;
     private final JwtTokenProvider jwtTokenProvider;
+    private final AdminInterceptor adminInterceptor;
 
-    public WebConfig(MemberDao memberDao, JwtTokenProvider jwtTokenProvider) {
+    public WebConfig(MemberDao memberDao, JwtTokenProvider jwtTokenProvider, AdminInterceptor adminInterceptor) {
         this.memberDao = memberDao;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.adminInterceptor = adminInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(memberDao, jwtTokenProvider));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/admin");
     }
 }
