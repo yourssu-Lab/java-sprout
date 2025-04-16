518af3ce72a1541c4de58c7ecb345e2bacb3bbd7
rover0811 <rover0811@hotmail.com>
2025-04-16 21:25:19 +0900

    [Feat] level3 implements HandlerInterceptor, admin check

--- src/main/java/com/yourssu/roomescape/WebConfig.java
+++ src/main/java/com/yourssu/roomescape/WebConfig.java
@@ -1,8 +1,10 @@
 package com.yourssu.roomescape;
 
+import com.yourssu.roomescape.member.AdminAuthInterceptor;
 import com.yourssu.roomescape.member.LoginMemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,13 +13,20 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final AdminAuthInterceptor adminAuthInterceptor;
 
-    public WebConfig(LoginMemberArgumentResolver loginMemberArgumentResolver) {
+    public WebConfig(LoginMemberArgumentResolver loginMemberArgumentResolver, AdminAuthInterceptor adminAuthInterceptor) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.adminAuthInterceptor = adminAuthInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminAuthInterceptor).addPathPatterns("/admin/**/");
     }
 }
