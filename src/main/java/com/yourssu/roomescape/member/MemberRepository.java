70a2b8bd67e38896ab6e5a159eccb14261f5259a
chlee <chlee@student.42seoul.kr>
2025-04-17 22:05:51 +0900

    add: find query to working login logic in website

--- src/main/java/com/yourssu/roomescape/member/MemberRepository.java
+++ src/main/java/com/yourssu/roomescape/member/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.yourssu.roomescape.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("SELECT m FROM Member m WHERE m.email = :email AND m.password = :password")
+
     Member findByEmailAndPassword(String email, String password);
 
     Member findByName(String name);
