3ba6966e7d305acca1d2a697abf7bf0c8d2805e9
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-04-18 19:58:32 +0900

    refactor: DTO class to record

--- src/main/java/com/yourssu/roomescape/auth/LoginRequest.java
+++ src/main/java/com/yourssu/roomescape/auth/LoginRequest.java
@@ -1,19 +1,4 @@
 package com.yourssu.roomescape.auth;
 
-public class LoginRequest {
-    private String email;
-    private String password;
-
-    public LoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+public record LoginRequest(String email, String password) {
 }
