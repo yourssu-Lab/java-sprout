44c73fc366e09c2b68fcdb01d9fb506d2bb225ab
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-04-18 12:29:21 +0900

    refactor: RowMapper 변수 분리

--- src/main/java/com/yourssu/roomescape/member/MemberDao.java
+++ src/main/java/com/yourssu/roomescape/member/MemberDao.java
@@ -1,83 +1,76 @@
-package com.yourssu.roomescape.member;
+    package com.yourssu.roomescape.member;
 
-import com.yourssu.roomescape.exception.CustomException;
-import com.yourssu.roomescape.exception.ErrorCode;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
+    import com.yourssu.roomescape.exception.CustomException;
+    import com.yourssu.roomescape.exception.ErrorCode;
+    import org.springframework.dao.DataAccessException;
+    import org.springframework.jdbc.core.JdbcTemplate;
+    import org.springframework.jdbc.core.RowMapper;
+    import org.springframework.jdbc.support.GeneratedKeyHolder;
+    import org.springframework.jdbc.support.KeyHolder;
+    import org.springframework.stereotype.Repository;
 
-@Repository
-public class MemberDao {
-    private JdbcTemplate jdbcTemplate;
+    @Repository
+    public class MemberDao {
+        private JdbcTemplate jdbcTemplate;
 
-    public MemberDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+        public MemberDao(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
 
-    public Member save(Member member) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            var ps = connection.prepareStatement("INSERT INTO member(name, email, password, role) VALUES (?, ?, ?, ?)", new String[]{"id"});
-            ps.setString(1, member.getName());
-            ps.setString(2, member.getEmail());
-            ps.setString(3, member.getPassword());
-            ps.setString(4, member.getRole());
-            return ps;
-        }, keyHolder);
+        private final RowMapper<Member> memberRowMapper = (rs, rowNum) -> new Member(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("email"),
+                rs.getString("role")
+        );
 
-        return new Member(keyHolder.getKey().longValue(), member.getName(), member.getEmail(), "USER");
-    }
+        public Member save(Member member) {
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> {
+                var ps = connection.prepareStatement("INSERT INTO member(name, email, password, role) VALUES (?, ?, ?, ?)", new String[]{"id"});
+                ps.setString(1, member.getName());
+                ps.setString(2, member.getEmail());
+                ps.setString(3, member.getPassword());
+                ps.setString(4, member.getRole());
+                return ps;
+            }, keyHolder);
 
-    public Member findByEmailAndPassword(String email, String password) {
-        try {
-            return jdbcTemplate.queryForObject(
-                    "SELECT id, name, email, role FROM member WHERE email = ? AND password = ?",
-                    (rs, rowNum) -> new Member(
-                            rs.getLong("id"),
-                            rs.getString("name"),
-                            rs.getString("email"),
-                            rs.getString("role")
-                    ),
-                    email, password
-            );
-        } catch (DataAccessException e) {
-            throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+            return new Member(keyHolder.getKey().longValue(), member.getName(), member.getEmail(), "USER");
+        }
+
+        public Member findByEmailAndPassword(String email, String password) {
+            try {
+                return jdbcTemplate.queryForObject(
+                        "SELECT id, name, email, role FROM member WHERE email = ? AND password = ?",
+                        memberRowMapper,
+                        email, password
+                );
+            } catch (DataAccessException e) {
+                throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+            }
+        }
+
+        public Member findByEmail(String email) {
+            try {
+                return jdbcTemplate.queryForObject(
+                        "SELECT id, name, email, role FROM member WHERE email = ?",
+                        memberRowMapper,
+                        email
+                );
+            } catch (DataAccessException e) {
+                throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+            }
+        }
+
+        public Member findByName(String name) {
+            try {
+                return jdbcTemplate.queryForObject(
+                        "SELECT id, name, email, role FROM member WHERE name = ?",
+                        memberRowMapper,
+                        name
+                );
+            } catch (DataAccessException e) {
+                throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+            }
         }
     }
-
-    public Member findByEmail(String email) {
-        try {
-            return jdbcTemplate.queryForObject(
-                    "SELECT id, name, email, role FROM member WHERE email = ?",
-                    (rs, rowNum) -> new Member(
-                            rs.getLong("id"),
-                            rs.getString("name"),
-                            rs.getString("email"),
-                            rs.getString("role")
-                    ),
-                    email
-            );
-        } catch (DataAccessException e) {
-            throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
-        }
-    }
-
-    public Member findByName(String name) {
-        try {
-            return jdbcTemplate.queryForObject(
-                    "SELECT id, name, email, role FROM member WHERE name = ?",
-                    (rs, rowNum) -> new Member(
-                            rs.getLong("id"),
-                            rs.getString("name"),
-                            rs.getString("email"),
-                            rs.getString("role")
-                    ),
-                    name
-            );
-        } catch (DataAccessException e) {
-            throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
-        }
-    }
-}
