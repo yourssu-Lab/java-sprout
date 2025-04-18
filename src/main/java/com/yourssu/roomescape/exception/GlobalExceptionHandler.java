15a68e8652c9515401fa8f61b678c63b3c6165fb
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-04-18 19:00:50 +0900

    refactor: @ExceptionHandler(Exception.class) 추가

--- src/main/java/com/yourssu/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yourssu/roomescape/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.yourssu.roomescape.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,8 +9,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleServerException(Exception e) {
+        log.warn(e.getMessage(), e);
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        ErrorResponse response = new ErrorResponse(errorCode.name(), errorCode.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.warn(e.getMessage(), e);
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = new ErrorResponse(errorCode.name(), errorCode.getMessage());
         return ResponseEntity.status(errorCode.getStatus()).body(response);
