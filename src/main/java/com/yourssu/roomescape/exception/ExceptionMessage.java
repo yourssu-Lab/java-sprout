5dd5f666873ec6217b2d192a3812ef715fba33f1
jeong724 <oliveyou724@gmail.com>
2025-04-19 17:31:01 +0900

    6단계 : 중복 예약 불가 예외

--- src/main/java/com/yourssu/roomescape/exception/ExceptionMessage.java
+++ src/main/java/com/yourssu/roomescape/exception/ExceptionMessage.java
@@ -6,6 +6,9 @@ public enum ExceptionMessage {
     NO_EXIST_TIME("시간이 존재하지 않습니다."),
     NO_EXIST_THEME("테마가 존재하지 않습니다."),
     NO_EXIST_RESERVATION("예약이 존재하지 않습니다."),
+    ALREADY_EXIST_RESERVATION("이미 예약이 존재합니다."),
+    CANCLE_DENY_RESERVATION("예약은 대기 상태일 때만 취소할 수 있습니다."),
+
     ;
     private final String message;
 
