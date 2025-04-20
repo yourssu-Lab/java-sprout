f0e9f5747556fee3b3dd47507c8ebb1aae6249bb
Changmin Lee <chlee@student.42seoul.kr>
2025-04-20 15:21:25 +0900

    Update src/main/java/com/yourssu/roomescape/member/MemberRepository.java
    Co-authored-by: Namgung Hyun <nggus5@gmail.com>

--- src/main/java/com/yourssu/roomescape/member/MemberRepository.java
+++ src/main/java/com/yourssu/roomescape/member/MemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("SELECT m FROM Member m WHERE m.email = :email AND m.password = :password")
-
     Member findByEmailAndPassword(String email, String password);
 
     Member findByName(String name);
