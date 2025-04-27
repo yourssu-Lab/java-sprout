1bcdc24a5c67c44289ff4143d001a7627047ed01
rover0811 <rover0811@hotmail.com>
2025-04-27 20:00:17 +0900

    [Add] JPA test for entity persistence and reservation retrieval
    Introduced JpaTest class to verify JPA entity persistence and API response handling. The tests ensure correct storage of Time entities and validate reservation retrieval functionality using a mock authentication token.

---
+++ src/test/java/com/yourssu/roomescape/JpaTest.java
@@ -0,0 +1,72 @@
+package com.yourssu.roomescape;
+
+
+import com.yourssu.roomescape.reservation.MyReservationResponse;
+import com.yourssu.roomescape.time.Time;
+import com.yourssu.roomescape.time.TimeRepository;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class JpaTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private TimeRepository timeRepository;
+
+    String createToken(String email, String password) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/login")
+                .then().log().all()
+                .statusCode(200)
+                .extract();
+
+        String token = response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
+        return token;
+    }
+
+    @Test
+    void 사단계() {
+        Time time = new Time("10:00");
+        entityManager.persist(time);
+        entityManager.flush();
+
+        Time persistTime = timeRepository.findById(time.getId()).orElse(null);
+
+        assertThat(persistTime.getValue()).isEqualTo(time.getValue());
+    }
+
+    @Test
+    void 오단계() {
+        String adminToken = createToken("admin@email.com", "password");
+
+        List<MyReservationResponse> reservations = RestAssured.given().log().all()
+                .cookie("token", adminToken)
+                .get("/reservations-mine")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList(".", MyReservationResponse.class);
+
+        assertThat(reservations).hasSize(3);
+    }
+
+}
