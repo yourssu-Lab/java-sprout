09cc8c48c41e5ad285c1429cbf742dee0af09cbb
rover0811 <rover0811@hotmail.com>
2025-04-27 20:00:35 +0900

    [Add] repositories and response class for reservation handling
    Implemented `ReservationRepository`, `ThemeRepository`, and `TimeRepository` to interact with corresponding entities. Introduced `MyReservationResponse` to encapsulate reservation data for API responses. These changes lay the groundwork for managing reservations and related data.

---
+++ src/main/java/com/yourssu/roomescape/reservation/ReservationRepository.java
@@ -0,0 +1,25 @@
+package com.yourssu.roomescape.reservation;
+
+import com.yourssu.roomescape.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByDateAndThemeId(String date, Long themeId);
+    List<Reservation> findByMember(Member member);
+    List<Reservation> findByMemberId(Long memberId);
+    @Query("SELECT COUNT(r) > 0 FROM Reservation r " +
+            "WHERE r.member.id = :memberId " +
+            "AND r.date = :date " +
+            "AND r.theme.id = :themeId " +
+            "AND r.time.id = :timeId")
+    boolean existsByMemberIdAndDateAndThemeIdAndTimeId(
+            @Param("memberId") Long memberId,
+            @Param("date") String date,
+            @Param("themeId") Long themeId,
+            @Param("timeId") Long timeId);
+
+}
