a2333e708bd82d4c5162829555741f5783e557c1
rover0811 <rover0811@hotmail.com>
2025-04-29 10:41:17 +0900

    [Move] AppConstants 클래스 패키지 이동, 마지막 줄 개행 해결
    AppConstants 클래스의 패키지를 common으로 이동하여 프로젝트 구조를 정리했습니다. 마지막 줄 개행을 주었습니다.

--- src/main/java/com/yourssu/roomescape/common/AppConstants.java
+++ src/main/java/com/yourssu/roomescape/common/AppConstants.java
@@ -1,19 +1,10 @@
-package com.yourssu.roomescape;
+package com.yourssu.roomescape.common;
 
-/**
- * Application-wide constants
- */
 public class AppConstants {
-    // Cookie names
     public static final String TOKEN_COOKIE_NAME = "token";
 
-    // Role names
     public static final String ROLE_ADMIN = "ADMIN";
 
-
-    // Other constants can be added here as needed
-
-    // Private constructor to prevent instantiation
     private AppConstants() {
         throw new AssertionError("Constants class should not be instantiated");
     }
