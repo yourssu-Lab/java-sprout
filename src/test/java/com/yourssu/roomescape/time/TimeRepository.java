3d6ab49106a124acfed86ad277dfe1a262387fd8
rover0811 <rover0811@hotmail.com>
2025-05-04 14:32:59 +0900

    [Remove] TimeRepository 삭제
    TimeRepository 인터페이스를 더 이상 사용하지 않아 코드베이스에서 제거했습니다. 불필요한 코드를 정리하여 유지보수성을 높였습니다.

--- src/test/java/com/yourssu/roomescape/time/TimeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yourssu.roomescape.time;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimeRepository extends JpaRepository<Time, Long> {
-    void deleteById(Long id);
-}
